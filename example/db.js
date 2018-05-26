dee6c7c3feb1a5bf607f850f6333a3f5da778ef9
Yefri <yefrigaitan@gmail.com>
2018-05-26 12:33:34 -0400

    Final project

--- example/db.js
+++ example/db.js
@@ -1,6 +1,10 @@
 module.exports = function() {
   return {
     profiles: require('./profiles.json'),
-    tweets: require('./tweets.json')
+    tweets1: require('./hashtagdogs.json'),
+    tweets2: require('./shortcuts.json'),
+    tweets3: require('./earlybird.json'),
+    tweets4: require('./hashtag_cats_are_evil.json'),
+    tweets5: require('./hashtag_hangry.json')
   }
 }
